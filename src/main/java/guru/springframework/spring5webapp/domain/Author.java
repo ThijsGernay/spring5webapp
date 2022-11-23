2853ebc9fbee359049efc525037c5106ad3ea693
Thijs <thijs.gernay@hotmail.com>
2022-11-23 12:52:21 +0100

    Assignment 1 + 2

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,31 @@
+package guru.springframework.spring5webapp.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String FirstName;
+    private String LastName;
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author(String firstName, String lastName) {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,33 @@
+package guru.springframework.spring5webapp.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String title;
+    private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
+    }
+}
